6a8c7c59e8ab076bcdd1f74d9e6ceeebb8cbb1a2
sandrom <sandro.motyl@gmail.com>
2020-07-01 21:57:53 -0300

    feat styles

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,12 @@ export const apikey = "79d54bf1dc1b1e6e281ec2deb7d0bf3e";
 const api = axios.create({
   baseURL: "https://m0na-bot-backend.herokuapp.com/",
   // baseURL: "http://localhost:5000",
+  // headers: {
+  //   "Access-Control-Allow-Origin": "*",
+  //   "Content-Type": "application/json",
+  //   Accept: "application/json",
+  //   methods: "GET",
+  // },
 });
 
 export default api;
