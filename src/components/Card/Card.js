20eb85ca9da2e8a1d2befa9541539329f8fcdaf6
sandrom <sandro.motyl@gmail.com>
2020-07-14 21:15:09 -0300

    add Dockerfile

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,60 +1,26 @@
-import React, { useEffect, useState } from "react";
-import api from "../../services/api";
+import React, { useState } from "react";
 
-// import * as style from "./Card.styles";
 import "./Card.style.css";
 
-// const mock = [
-//   {
-//     link:
-//       "https://www.al.sp.gov.br/propositura/acessorio/?idDocumento=1000326140&tpDocumento=14",
-//     description:
-//       "Emenda de Pauta 811/2020, de 02/06/2020 - Projeto de lei 307/2020",
-//   },
-//   {
-//     link:
-//       "https://www.al.sp.gov.br/propositura/acessorio/?idDocumento=1000324752&tpDocumento=14",
-//     description:
-//       "Emenda de Pauta 231/2020, de 22/05/2020 - Projeto de lei 350/2020",
-//   },
-//   {
-//     link:
-//       "https://www.al.sp.gov.br/propositura/acessorio/?idDocumento=1000324751&tpDocumento=14",
-//     description:
-//       "Emenda de Pauta 230/2020, de 22/05/2020 - Projeto de lei 350/2020",
-//   },
-// ];
-
-export function Card({ title, entity }) {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+export function Card({ data }) {
   const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    async function getData() {
-      setLoading(true);
-      const response = await api.get(`/${entity}`);
-      setLoading(false);
-      setData(response.data);
-    }
-
-    getData();
-  }, [entity]);
-
-  if (loading) return <p className="text">loading {entity}</p>;
 
   return (
     <>
       <div id="card" className="card" onClick={() => setShow(!show)}>
         <p className="text">
-          <strong>{title.toUpperCase()}</strong> ({data?.length})
+          <strong>{data.grupo.toUpperCase()}</strong> ({data.list.length})
         </p>
       </div>
       {show && (
         <ul id="document_list" className="document_list">
-          {data.map((document) => (
-            <li id="document_item" className="document_item">
-              <a className="link" href={`${document.link}`}>
+          {data.list.map((document) => (
+            <li
+              key={document.url + document.description}
+              id="document_item"
+              className="document_item"
+            >
+              <a className="link" href={`${document.url}`}>
                 {document.description}
               </a>
             </li>
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Card from "../../components/Card";
+import api from "../../services/api";
 
 import logo from "../../assets/logo_400x400.jpg";
 import * as style from "./Main.styles";
 
+export function groupBy(array, field) {
+  const dadosAgrupados = Object.keys(getKeys(array, field))
+    .map((descricaoGrupo) => {
+      return array.reduce((result, campo) => {
+        if (campo[field] === descricaoGrupo) result.push(campo);
+        return result;
+      }, []);
+    })
+    .map((grupoList) => {
+      return { grupo: grupoList[0][field], list: grupoList };
+    });
+
+  return dadosAgrupados;
+}
+
+function getKeys(xs, key) {
+  return xs.reduce(function (result, current) {
+    (result[current[key]] = result[current[key]] || []).push(current);
+    return result;
+  }, {});
+}
+
 export function Main() {
+  const [data, setData] = useState([]);
+  const [date, setDate] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleClickRobots() {
+    setLoading(true);
+    const response = await api.get(`http://67.205.169.66:8080/all_robots`);
+    setLoading(false);
+    setData(response.data);
+
+    localStorage.setItem("m0na_documents", JSON.stringify(response.data));
+    localStorage.setItem("m0na_date", JSON.stringify(new Date()));
+  }
+
+  useEffect(() => {
+    const storageData = localStorage.getItem("m0na_documents");
+    const storageDate = localStorage.getItem("m0na_date");
+
+    if (storageData) {
+      setData(JSON.parse(storageData));
+      setDate(storageDate);
+    }
+  }, []);
+
   return (
     <div style={style.main_content}>
+      <p>{date}</p>
+      <button onClick={handleClickRobots}>START ROBOTS</button>
       <img style={style.m0na_logo} src={logo} alt="m0na-bot logo" />
-      <Card entity="alesp" title="São Paulo" />
-      {/* <Card entity="ales" title="Espírito Santo" /> */}
-      {/* <Card entity="alepr" title="Paraná" /> */}
-      {/* <Card entity="alba" title="Bahia" /> */}
+      {loading ? <p>loading</p> : null}
+      {groupBy(data, "aka").map((d) => (
+        <Card key={d.grupo} data={d} />
+      ))}
     </div>
   );
 }
